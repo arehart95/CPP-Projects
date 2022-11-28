80b04ee7a7d00a0d6e0472be070ae2cdda98f3d4
Quentin Arehart <104207952+quentin-arehart@users.noreply.github.com>
2022-11-28 12:49:44 +0900

    Create incrementcalculation.cpp
    Calculates an increase in salary based on appraisal rating.

---
+++ Control Structures/incrementcalculation.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+
+int main()   //DO NOT change the 'main' signature
+{
+    int salary;
+    int appraisedSalary = 0;
+    float appraisalRating;
+    
+    cout << "Enter the salary" << endl;
+    cin >> salary;
+    
+    cout << "Enter the Performance appraisal rating" << endl;
+    cin >> appraisalRating;
+    
+    if (appraisalRating >= 1 && appraisalRating <= 3) {
+        appraisedSalary += salary * .10 + salary;
+        cout << appraisedSalary << endl;
+    } else if (appraisalRating >= 3.1 && appraisalRating <= 4) {
+        appraisedSalary += salary * .25 + salary;
+        cout << appraisedSalary << endl;
+    } else if (appraisalRating >= 4.1 && appraisalRating <= 5) {
+        appraisedSalary = salary * .30 + salary;
+        cout << appraisedSalary << endl;
+    } else {
+        cout << "Invalid Input" << endl;
+        
+    }
+  
+  return 0;
+    
+}
