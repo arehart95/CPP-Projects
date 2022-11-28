d4206d18beb1f783e3a36c8343c72d4d9f243081
Quentin Arehart <104207952+quentin-arehart@users.noreply.github.com>
2022-11-28 13:36:43 +0900

    Create productdetails.cpp
    Work in progress!

---
+++ Structures/productdetails.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+struct Product
+{
+    string name;
+    float price;
+    int quantity;
+    
+    
+    Product readitem()
+    {
+        
+        
+    } 
+    void printitem()
+    {
+        float totalAmount;
+        totalAmount = quantity * price;
+        
+    } 
+};
+
+
+int main()
+{
+   
+   
+    
+    
+    return 0;
+    
+}
