918a810f31f1775283a17d80f480496a38b167b7
Quentin Arehart <104207952+quentin-arehart@users.noreply.github.com>
2022-11-28 13:11:16 +0900

    Create emailvalidation.cpp
    Checks if an entered email address is valid or not.

---
+++ Pointers/emailvalidation.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+bool is_valid(string email)
+{
+    int At = 1, Dot = -1;
+    
+    for (int i = 0; i < email.length(); i++) 
+    {
+        if (email[i] == '@') {
+            At = i;
+        } else if (email[i] == '.') {
+            Dot = i;
+        }
+        
+    }
+    
+    if (At == -1 || Dot == -1) {
+        return 0;
+    } 
+    if (At > Dot) {
+        return 0;
+    }
+    return !(Dot >= (email.length() - 1));
+}
+
+
+
+int main()   //DO NOT change the 'main' signature
+{
+    string email;
+    getline(cin, email);
+    
+    bool ans = is_valid(email);
+    
+    if (ans) {
+        cout << "Valid email ID" << endl;
+    } else {
+        cout << "Invalid email ID" << endl;
+    }
+  
+  return 0;
+}
