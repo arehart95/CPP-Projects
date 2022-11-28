5cbd42caa7738b3eb517139b324f90a909e4e8cd
Quentin Arehart <104207952+quentin-arehart@users.noreply.github.com>
2022-11-28 12:38:36 +0900

    Create librarybooks.cpp
    Maintaing a log for libary books.

---
+++ Operators/librarybooks.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+int main()  
+{
+    string book_name, author_name;
+    cout << "Enter two strings:" << endl;
+    cin >> book_name >> author_name;
+    cout << book_name << " was written by " << author_name << endl;
+    
+    return 0;
+}
