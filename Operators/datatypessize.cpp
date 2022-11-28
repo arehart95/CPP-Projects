4ab57a64ead1e325f4cbd54ea0e763b17b0ae5cb
Quentin Arehart <104207952+quentin-arehart@users.noreply.github.com>
2022-11-28 12:40:03 +0900

    Create datatypessize.cpp
    Prints the size of data types in C++

---
+++ Operators/datatypessize.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main()   //DO NOT change the 'main' signature
+{
+    cout << "Size of Integer = 4 bytes" << endl;
+    cout << "Size of Floating point = 4 bytes" << endl;
+    cout << "Size of double = 8 bytes" << endl;
+    cout << "Size of character = 1 byte" << endl;
+    cout << "Size of Long integer = 8 bytes" << endl;
+    cout << "Size of Short integer = 2 bytes" << endl;
+  
+  return 0;
+}
