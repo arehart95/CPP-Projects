533a610a5d22b0c495a51b6ced7e48821f686b42
Quentin Arehart <104207952+arehart95@users.noreply.github.com>
2024-12-03 15:23:36 -0500

    Add files via upload

---
+++ VingenereApp.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include </home/quentin/Downloads/vigenere.h>
+using namespace std;
+
+int main() {
+
+    cout << "Welcome to the encryption/decryption station\n";
+    cout << "Please enter your selection\n";
+    cout << "1. Encryption\n2. Decryption\n3. Exit" << endl;
+
+    int selection;
+    cin >> selection;
+
+    switch (selection) {
+        case 1: {
+            cout << "Please enter the text you'd like to encrypt: \n";
+            string plainText;
+            cin >> plainText;
+            cout << "Please enter the text you'd like to use for a key: \n";
+            string key;
+            cin >> key;
+            Vigenere encrypt;
+            encrypt.makeUpperCase(plainText, key);
+            const string newKey = encrypt.generateKey(plainText, key);
+            cout << "Generating encrypted text..." << endl;
+            cout << encrypt.cipherText(plainText, newKey);
+            break;
+        }
+        case 2: {
+            cout << "Please enter the text you'd like to decrypt: \n";
+            string cipherText;
+            cin >> cipherText;
+            cout << "Please enter the text you'd like to use for a key: \n";
+            string key;
+            cin >> key;
+            Vigenere decrypt;
+            decrypt.makeUpperCase(cipherText, key);
+            const string newKey = decrypt.generateKey(cipherText, key);
+            cout << "Generating decrypted text..." << endl;
+            cout << decrypt.originalText(cipherText, newKey);
+        }
+        case 3: {
+            break;
+        }
+        default:
+            cout << "Invalid selection\n";
+    }
+}
---
+++ vigenere.h
@@ -0,0 +1,63 @@
+#ifndef VIGENERE_H
+#define VIGENERE_H
+#include <string>
+#include <algorithm>
+using namespace std;
+
+class Vigenere {
+    string _plainText, _key, _cipherText;
+public:
+    //Vigenere(const Vigenere&) = delete;
+    string generateKey(const string& text, string key) {
+        this->_key = key;
+        this->_plainText = text;
+        const int x = text.size();
+
+        for (int i = 0; ; i++) {
+            if (x == i)
+                i = 0;
+            if (key.size() == text.size())
+                break;
+            key.push_back(key[i]);
+        }
+        return key;
+    }
+
+
+     string cipherText(const string& text, const string &key) {
+        this->_plainText = text;
+        string cipherText;
+
+            for (int i = 0; i < text.size(); i++) {
+                char c = (text[i] + key[i]) % 26;
+                c += 'A';
+                cipherText.push_back(c);
+            }
+            return cipherText;
+        }
+
+
+     string originalText(const string& cipherText, const string &key) {
+         this->_cipherText = cipherText;
+            string originalText;
+
+            for (int i = 0; i < cipherText.size(); i++) {
+                char c = (cipherText[i] - key[i] + 26) % 26;
+                c += 'A';
+                originalText.push_back(c);
+            }
+            return originalText;
+        }
+
+    void makeUpperCase(string& plainText, string& key) {
+        this->_plainText = plainText;
+        this->_key = key;
+        if (any_of(plainText.begin(), plainText.end(), ::islower)) {
+            transform(plainText.begin(), plainText.end(), plainText.begin(), ::toupper);
+        }
+        if (any_of(key.begin(), key.end(), ::islower)) {
+            transform(key.begin(), key.end(), key.begin(), ::tolower);
+        }
+ }
+};
+#endif //VIGENERE_H
